c432836b80b3b77efd6d0391d1d0a492079d1109
vishal-code-create <vc110497@gmail.com>
2024-08-02 17:02:18 +0530

    spring basic project

---
+++ src/main/java/com/crio/leaderboard/management/LeaderboardManagementApplication.java
@@ -0,0 +1,14 @@
+package com.crio.leaderboard.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(LeaderboardManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crio/leaderboard/management/LeaderboardManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crio.leaderboard.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderboardManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
